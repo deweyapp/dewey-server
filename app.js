090f3d52c9dffbc85aac352ae15a2b4305c9ca51
Artem Grygor <artemgrygor@gmail.com>
2015-03-08 14:53:24 -0400

    init commit for dewey-server

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+
+var cors = function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-Access-Token, X-Revision, Content-Type');
+
+	next();
+};
+
+app.configure(function(){
+	app.use(express.json());
+	app.use(express.urlencoded());
+	app.use(express.cookieParser());
+	app.use(cors);
+	app.use(express.methodOverride());
+	app.use(app.router);
+});
+
+require('./source/router')(app);
+
+var port = process.env.port || 3000;
+http.createServer(app).listen(port, function() {
+});
